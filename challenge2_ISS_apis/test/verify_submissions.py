776c57c8ef2d7192c2a30044c1e42fe1ecbfc605
Gabriele Bonetti <gabriele.bonetti@nokia.com>
2022-06-12 18:19:57 +0200

    updated requirements verifier and challenge template for challenge2 iss

--- challenge2_ISS_apis/test/verify_submissions.py
+++ challenge2_ISS_apis/test/verify_submissions.py
@@ -1,9 +1,14 @@
 import os
 from pathlib import Path
 
+import requests
+
 challenge_dir = Path(os.path.dirname(__file__)).parent
-solution_file = challenge_dir / "solutions/passphrase.txt"
+solution_file = challenge_dir / "solutions/iss.py"
 assert solution_file.exists(), "No solution file found"
 
 if __name__ == "__main__":
-    pass
+    from challenge2_ISS_apis.solutions.iss import calculate_speed
+    calculated_speed = calculate_speed()
+    real_speed = requests.get("https://api.wheretheiss.at/v1/satellites/25544").json()["velocity"]
+    assert real_speed * 0.9 < calculated_speed < real_speed * 1.1, f"incorrect speed: {calculated_speed}"
