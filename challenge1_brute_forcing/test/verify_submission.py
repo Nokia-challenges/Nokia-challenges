7c841b421750610c08251175ac416dd25c3b7cf9
Gabriele Bonetti <gabriele.bonetti@nokia.com>
2022-06-12 16:54:10 +0200

    add pwd and fix validator

--- challenge1_brute_forcing/test/verify_submission.py
+++ challenge1_brute_forcing/test/verify_submission.py
@@ -4,7 +4,8 @@ from pathlib import Path
 challenge_dir = Path(os.path.dirname(__file__)).parent
 solution_file = challenge_dir / "solutions/passphrase.txt"
 
-with open(solution_file, "r") as f:
-    passphrase = f.read()
+if __name__ == '__main__':
+    with open(solution_file, "r") as f:
+        passphrase = f.read()
 
-assert passphrase == 42
+    assert passphrase == str(42)
