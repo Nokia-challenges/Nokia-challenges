62916fe8b19c7cbdeb9f08485a757c67ab95d4cc
Gabriele Bonetti <gabriele.bonetti@nokia.com>
2022-06-12 23:24:46 +0200

    finalize first challenge part 2

--- challenge1_brute_forcing/test/verify_submissions.py
+++ challenge1_brute_forcing/test/verify_submissions.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 
 challenge_dir = Path(os.path.dirname(__file__)).parent
-solution_file = challenge_dir / "solutions/passphrase.txt"
+solution_file = challenge_dir / "passphrase.txt"
 assert solution_file.exists(), "No solution file found"
 
 if __name__ == "__main__":
