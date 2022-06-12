8a6365cdefa72ce85e65dcf29b2a43be439d748a
Federico Micelli <micelli.frederick@gmail.com>
2022-06-12 14:26:29 +0200

    Morse encryption method

---
+++ challenge3_RPi_sensors/morse.py
@@ -0,0 +1,27 @@
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+# method to encrypt the message (from italian to morse)
+def encrypt_morse(message):
+    message = message.upper()
+    morse_code = ''
+    for char in message:
+        if char == ' ':
+            # 2 blank spaces to separate words
+            morse_code += '  '
+        else:
+            morse_code += MORSE_CODE_DICT[char] + ' '  # char are separated by a single blank space
+    return morse_code
