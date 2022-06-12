36c735fb350e13bce46e4e06769abdf9c90ccd6b
Gabriele Bonetti <gabriele.bonetti@nokia.com>
2022-06-12 17:06:24 +0200

    use hashed password

--- challenge1_brute_forcing/test/verify_submission.py
+++ challenge1_brute_forcing/test/verify_submission.py
@@ -1,11 +1,14 @@
+import hashlib
 import os
 from pathlib import Path
 
 challenge_dir = Path(os.path.dirname(__file__)).parent
 solution_file = challenge_dir / "solutions/passphrase.txt"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with open(solution_file, "r") as f:
         passphrase = f.read()
 
-    assert passphrase == str(42)
+    assert (
+        hashlib.sha224(passphrase.encode('utf-8')).hexdigest() == "33624935aa346e032eaa4b634f76b1e4a2f5b4b52f5d4b49d4b466c7"
+    ), f"incorrect passphrase: {passphrase}"
