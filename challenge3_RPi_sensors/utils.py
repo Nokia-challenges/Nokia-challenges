8d02a4d4af6e3556347a3acc71b5ae8e810b7b69
Federico Micelli <federico.micelli@nokia.com>
2022-06-13 18:13:45 +0200

    hash values updated in checks

--- challenge3_RPi_sensors/utils.py
+++ challenge3_RPi_sensors/utils.py
@@ -4,5 +4,5 @@ import hashlib
 def check_password(pwd):
     return (
             hashlib.sha256(pwd.encode("utf-8")).hexdigest()
-            == '8b01e15594a3790356409c685c434520bf88b2dbc71e20192f84e497b63dfaf5'
+            == 'a8df85f48fda63ef483c4b9c784254e34198036a2aeb44553020ca654acdab46'
     )
