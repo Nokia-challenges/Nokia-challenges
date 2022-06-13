74a83a4267c079a6c4b14ca210f2aefe4f0ed0bb
Gabriele Bonetti <gabriele.bonetti@nokia.com>
2022-06-13 18:38:56 +0200

    use python -m black reformat remove unused solutions folder

--- challenge1_brute_forcing/utils.py
+++ challenge1_brute_forcing/utils.py
@@ -3,6 +3,6 @@ import hashlib
 
 def check_password(pwd):
     return (
-            hashlib.sha256(pwd.encode("utf-8")).hexdigest()
-            == '8b01e15594a3790356409c685c434520bf88b2dbc71e20192f84e497b63dfaf5'
+        hashlib.sha256(pwd.encode("utf-8")).hexdigest()
+        == "8b01e15594a3790356409c685c434520bf88b2dbc71e20192f84e497b63dfaf5"
     )
--- challenge3_RPi_sensors/utils.py
+++ challenge3_RPi_sensors/utils.py
@@ -3,6 +3,6 @@ import hashlib
 
 def check_password(pwd):
     return (
-            hashlib.sha256(pwd.encode("utf-8")).hexdigest()
-            == 'a8df85f48fda63ef483c4b9c784254e34198036a2aeb44553020ca654acdab46'
+        hashlib.sha256(pwd.encode("utf-8")).hexdigest()
+        == "a8df85f48fda63ef483c4b9c784254e34198036a2aeb44553020ca654acdab46"
     )
