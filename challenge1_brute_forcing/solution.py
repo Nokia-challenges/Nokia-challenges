40f2bbd7680d074cca52dfd32ecd239c4c69609c
davideperegoo <davide.perego01@liceobanfi.eu>
2022-06-14 12:27:18 +0200

    solving "challenge1_brute_force"

--- challenge1_brute_forcing/solution.py
+++ challenge1_brute_forcing/solution.py
@@ -1,5 +1,23 @@
+from utils import check_password
+
+
+
 def run():
-    pass
+    dizionario = open("660000_parole_italiane.txt", "r")
+    passphrase = open("passphrase.txt", "w")
+
+    for parola in dizionario.readlines():
+        parola = parola[0:-1]
+        
+        if check_password(parola):
+
+            print(parola)
+
+            passphrase.write(parola)
+            return 0
+
+
+
 
 
 if __name__ == "__main__":
