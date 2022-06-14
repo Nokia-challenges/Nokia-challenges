2e3611b54a21643db6bacaac1571089ea8c0f945
LetiziaSala <letizia.sala@liceobanfi.eu>
2022-06-14 12:31:34 +0200

    reformattedfile

--- challenge1_brute_forcing/solution.py
+++ challenge1_brute_forcing/solution.py
@@ -1,11 +1,12 @@
 from utils import check_password
 
+
 def run():
-    dizionario = open("660000_parole_italiane_ok.txt","r")
+    dizionario = open("660000_parole_italiane_ok.txt", "r")
     f2 = open("passphrase.txt", "w")
 
     for parola in dizionario.readlines():
-        parola=parola[0:-1]
+        parola = parola[0:-1]
 
         if check_password(parola):
             print(parola)
