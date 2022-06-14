d0fe6a30b3517f9c6fb4bb2f473cea10894a191e
Lorenzo-Sangalli <Lorenzo.sangalli04@gmail.com>
2022-06-14 12:26:32 +0200

    soluzione prima challenge

--- challenge1_brute_forcing/solution.py
+++ challenge1_brute_forcing/solution.py
@@ -1,5 +1,20 @@
+from operator import truediv
+
+from utils import check_password
+
+
 def run():
-    pass
+    f = open("660000_parole_italiane_ok.txt")
+    f2 = open("passphrase.txt", 'w')
+
+    for pwd in f.readlines():
+        pwd=pwd[0:-1]
+        
+        if check_password(pwd):
+
+            f2.write(pwd)
+            return 0
+
 
 if __name__ == "__main__":
     run()
