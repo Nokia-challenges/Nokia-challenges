52ee98c8749c57b897445a33613386cbd8ef1e77
LetiziaSala <letizia.sala@liceobanfi.eu>
2022-06-14 15:32:06 +0200

    challenge2

--- challenge2_ISS_apis/solution.py
+++ challenge2_ISS_apis/solution.py
@@ -1,8 +1,28 @@
 url = "http://api.open-notify.org/iss-now.json"
 
+import requests
+import time
+import geopy.distance
+
 
 def run():
-    pass
+    r= requests.get(url).json()
+    pos={r["iss_position"]["latitude"],r["iss_position"]["longitude"]}
+    tempo=["timestamp"]
+
+    time.sleep(5)
+
+    r= requests.get(url).json()
+    pos2={r["iss_position"]["latitude"],r["iss_position"]["longitude"]}
+    tempo={"timestamp"}
+
+    timediff=tempo-tempo
+    RIS=int(geopy.great_circle(pos2,pos))
+
+    velocita=RIS/timediff
+
+    return velocita
+
 
 
 if __name__ == "__main__":
