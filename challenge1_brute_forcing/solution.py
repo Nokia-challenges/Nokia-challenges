87eb5e5f5223998ee52e9f50889c712c1bd3f72f
davideperegoo <davide.perego01@liceobanfi.eu>
2022-06-14 12:30:39 +0200

    formattato meglio

--- challenge1_brute_forcing/solution.py
+++ challenge1_brute_forcing/solution.py
@@ -1,23 +1,19 @@
 from utils import check_password
 
 
-
 def run():
     dizionario = open("660000_parole_italiane.txt", "r")
     passphrase = open("passphrase.txt", "w")
 
     for parola in dizionario.readlines():
         parola = parola[0:-1]
-        
+
         if check_password(parola):
 
             print(parola)
 
             passphrase.write(parola)
             return 0
-
-
-
 
 
 if __name__ == "__main__":
