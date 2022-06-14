5899ca9d5fcbab5602fbad2c48f8a7383c01816a
bevocitofoni <bevocitofoni@gmail.com>
2022-06-14 12:26:00 +0200

    risolta challenge 1

--- challenge1_brute_forcing/solution.py
+++ challenge1_brute_forcing/solution.py
@@ -1,6 +1,23 @@
+from operator import truediv
+
+from utils import check_password
+
+
 def run():
-    pass
+    f = open("660000_parole_italiane_ok.txt")
+    f2 = open("passphrase.txt", "w")
+
+    for password in f.readlines():
+        
+        password = password[:-1]
+#        print(password)
+
+        if check_password(password):
+            f2.write(password)
+            return 0;
+
 
 
 if __name__ == "__main__":
     run()
+
