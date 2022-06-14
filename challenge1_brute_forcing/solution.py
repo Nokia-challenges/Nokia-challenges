a522c3007c626969188c16330431f9372acf4ec4
LetiziaSala <letizia.sala@liceobanfi.eu>
2022-06-14 12:27:31 +0200

    soluzione challenge_brute_force

--- challenge1_brute_forcing/solution.py
+++ challenge1_brute_forcing/solution.py
@@ -1,4 +1,19 @@
+from utils import check_password
+
 def run():
+    dizionario = open("660000_parole_italiane_ok.txt","r")
+    f2 = open("passphrase.txt", "w")
+
+    for parola in dizionario.readlines():
+        parola=parola[0:-1]
+
+        if check_password(parola):
+            print(parola)
+
+            f2.write(parola)
+            return 0
+
+    print("")
     pass
 
 
