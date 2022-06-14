22c0320e996fa37e014bac525dc74101d5e2f25a
Lorenzo-Sangalli <Lorenzo.sangalli04@gmail.com>
2022-06-14 15:03:09 +0200

    prima soluzione della seconda challenge

--- challenge2_ISS_apis/solution.py
+++ challenge2_ISS_apis/solution.py
@@ -1,8 +1,30 @@
+import geopy
+import requests
+import time
+
+from geopy.distance import great_circle
+
 url = "http://api.open-notify.org/iss-now.json"
 
 
 def run():
-    pass
+    u = requests.get(url).json()
+
+    lat1 = u["iss_position"]["latitude"]
+    lon1 = u["iss_position"]["longitude"]
+
+    time.sleep(3.6)
+
+    u = requests.get(url).json()
+
+    lat2 = u["iss_position"]["latitude"]
+    lon2 = u["iss_position"]["longitude"]
+
+    pos1 = (lat1, lon1)
+    pos2 = (lat2, lon2)
+
+    ris = great_circle(pos1, pos2).km * 1000
+    return ris
 
 
 if __name__ == "__main__":
