539a296a1f8daed57524542b252cce133137a18e
bevocitofoni <bevocitofoni@gmail.com>
2022-06-14 15:12:10 +0200

    soluzione2 challenge2

--- challenge2_ISS_apis/solution.py
+++ challenge2_ISS_apis/solution.py
@@ -9,15 +9,19 @@ url = "http://api.open-notify.org/iss-now.json"
 
 def run():
     info1 = requests.get(url).json()
-    time.sleep(3.6)
+    time.sleep(5)
     info2 = requests.get(url).json()
 
     pos1 = (info1["iss_position"]["latitude"], info1["iss_position"]["longitude"])
     pos2 = (info2["iss_position"]["latitude"], info2["iss_position"]["longitude"])
 
-    dist = distance.great_circle(pos1, pos2).km
+    timedif = info2["timestamp"] - info1["timestamp"]
 
-    velocity = dist * 1000
+    dist = distance.great_circle(pos1, pos2).km * 1000
+
+    velocity = 3.6 * dist / timedif
+    print(velocity)
+
     return velocity
 
 
