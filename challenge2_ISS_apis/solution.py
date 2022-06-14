ddadc5b2a06b84b68bfbb2406dc55d4baef4bb3a
davideperegoo <davide.perego01@liceobanfi.eu>
2022-06-14 15:02:36 +0200

    solution to challenge2_ISS_apis

--- challenge2_ISS_apis/solution.py
+++ challenge2_ISS_apis/solution.py
@@ -1,8 +1,27 @@
+import geopy.distance as gp
+import requests
+
+import time
+
+
 url = "http://api.open-notify.org/iss-now.json"
 
 
 def run():
-    pass
+    idk = requests.get(url).json()
+
+    pos1 = (idk["iss_position"]["latitude"], idk["iss_position"]["longitude"])
+
+    time.sleep(3.6)
+
+    idk = requests.get(url).json()
+
+    pos2 = (idk["iss_position"]["latitude"], idk["iss_position"]["longitude"])
+
+    ris = int(1000 * gp.great_circle(pos2, pos1).kilometers)
+
+    #    print(ris)
+    return ris
 
 
 if __name__ == "__main__":
