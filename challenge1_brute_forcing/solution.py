9f15b9e4043947c23daa57c47ddbbddbd33551c2
saragambino <saragambino30@gmail.com>
2023-06-20 15:17:15 +0200

    plug

--- challenge1_brute_forcing/solution.py
+++ challenge1_brute_forcing/solution.py
@@ -1,5 +1,19 @@
+from utils import *
+import requests
+
 def run():
-    pass
+    response = requests.get("https://raw.githubusercontent.com/napolux/paroleitaliane/master/paroleitaliane/660000_parole_italiane.txt")
+
+    parole_attaccate = response.content
+    parole = parole_attaccate.splitlines()
+    miofile=open("passphrase.txt","w")
+
+    for i in range(len(parole)):
+        if check_password(parole[i].decode())==True:
+            miofile.write(parole[i].decode())
+
+
+
 
 
 if __name__ == "__main__":
