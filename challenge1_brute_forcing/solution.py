1629c6338fab15cefc97aa2f08969da2c110037a
Niccolo Pagano <niccolo.pagano@studentilicei.leonexiii.it>
2023-06-20 12:41:11 +0000

    bella fra

--- challenge1_brute_forcing/solution.py
+++ challenge1_brute_forcing/solution.py
@@ -1,6 +1,12 @@
-def run():
-    pass
+import utils 
 
+def run():
+    with open('660000_parole_italiane.txt', 'r') as f:
+        for line in f:
+            if utils.check_password(line[:-1]):
+                with open('passphrase.txt', 'w') as f:
+                    f.write(line[:-1])
+                break
 
 if __name__ == "__main__":
     run()
