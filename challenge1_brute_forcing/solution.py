c070296ee82f0843a99e3a26d9e5491d5342c826
unknown <lindaniele25@gmail.com>
2023-06-20 15:05:45 +0200

    La mia soluzione

--- challenge1_brute_forcing/solution.py
+++ challenge1_brute_forcing/solution.py
@@ -1,5 +1,16 @@
+from utils import check_password
+
+
 def run():
-    pass
+    """
+    Reads the italian dictionary line by line, 
+    strips the line and checks if it corresponds with the hash.
+    """
+    with open("660000_parole_italiane.txt") as file:
+        for line in file:
+            if check_password(line[:-1]):
+                return line[:-1]
+            
 
 
 if __name__ == "__main__":
