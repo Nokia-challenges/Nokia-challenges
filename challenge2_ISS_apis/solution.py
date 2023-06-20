0cc0a8f78fc0be639c012dee1b6c50c61c001d81
FrancescoCasasole <francesco.casasole@gmail.com>
2023-06-20 16:54:59 +0200

    primo tentativo

--- challenge2_ISS_apis/solution.py
+++ challenge2_ISS_apis/solution.py
@@ -1,7 +1,29 @@
 url = "http://api.open-notify.org/iss-now.json"
-
+import geopy
+import time
+import requests
+from geopy import distance, Point
 
 def run():
+    t = 5
+
+    response = requests.get(url)
+    linea = response.json()
+    lon1 = linea["iss_position"]["longitude"]
+    lat1 = linea["iss_position"]["latitude"]
+    time.sleep(t)
+    response = requests.get(url)
+    linea = response.json()
+    lon2 = linea["iss_position"]["longitude"]
+    lat2 = linea["iss_position"]["latitude"]
+
+    p1 = Point(lat1, lon1)
+    p2 = Point(lat2, lon2)
+
+    dist = distance.distance(p1,p2).km
+
+    v = dist/(t/3600)
+    print(v)
     pass
 
 
