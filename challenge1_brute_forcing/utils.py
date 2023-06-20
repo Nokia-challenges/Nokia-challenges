8558f28657a4d9963b012f10d19a0cc287d9d6f1
FrancescoCasasole <francesco.casasole@gmail.com>
2023-06-20 15:27:44 +0200

    seconda tentativo soluzione

--- challenge1_brute_forcing/utils.py
+++ challenge1_brute_forcing/utils.py
@@ -3,6 +3,5 @@ import hashlib
 
 def check_password(pwd):
     return (
-        hashlib.sha256(pwd.encode("utf-8")).hexdigest()
-        == "44aeafa65f8b10498c3de32e5789633cdd73732a09494cd4baf7ec2197af33b7"
+        hashlib.sha256(pwd.encode("utf-8")).hexdigest() == "44aeafa65f8b10498c3de32e5789633cdd73732a09494cd4baf7ec2197af33b7"
     )
