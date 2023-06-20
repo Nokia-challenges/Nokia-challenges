bcd1a500d873472fac4a719ac80c2f6ada894071
FrancescoCasasole <francesco.casasole@gmail.com>
2023-06-20 14:55:07 +0200

    primo tentativo soluzione

--- challenge1_brute_forcing/solution.py
+++ challenge1_brute_forcing/solution.py
@@ -1,4 +1,18 @@
+import urllib.request
+
+url = "https://github.com/napolux/paroleitaliane/blob/master/paroleitaliane/660000_parole_italiane.txt"
+from utils import check_password
+
+
 def run():
+    with urllib.request.urlopen(url) as response:
+        while True:
+            html = response.readline()
+            if check_password(html) == True:
+                file_uno = open("passphrase.txt", "w")
+                file_uno.write(html)
+                file_uno.close()
+
     pass
 
 
