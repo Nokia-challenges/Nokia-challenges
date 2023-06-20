c6d0282ee1fe12f04b587348ea528401761f8138
aleBeretta <beretta.alessandro06@gmail.com>
2023-06-20 16:59:18 +0200

    soluzione1

--- challenge2_ISS_apis/solution.py
+++ challenge2_ISS_apis/solution.py
@@ -2,22 +2,26 @@ url = "http://api.open-notify.org/iss-now.json"
 import requests, time, json
 from geopy import distance
 
+
 def run():
-    response1=requests.get(url)
-    dati1=response1.json()["iss_position"]
-    lat1, lon1=float(dati1["latitude"]), float(dati1["longitude"])
+    response1 = requests.get(url)
+    dati1 = response1.json()["iss_position"]
+    lat1, lon1 = float(dati1["latitude"]), float(dati1["longitude"])
     print(lat1, lon1)
     time.sleep(6)
-    response2=requests.get(url)
-    dati2=response2.json()["iss_position"]
-    lat2, lon2=float(dati2["latitude"]), float(dati2["longitude"])
+    response2 = requests.get(url)
+    dati2 = response2.json()["iss_position"]
+    lat2, lon2 = float(dati2["latitude"]), float(dati2["longitude"])
     print(lat2, lon2)
 
-    pos1=(lat1,lon1)
-    pos2=(lat2, lon2)
-    dist=distance.distance(pos1, pos2).km
-    vel=dist*600
+    pos1 = (lat1, lon1)
+    pos2 = (lat2, lon2)
+    dist = distance.distance(pos1, pos2).km
+    vel = dist * 600
+    print(vel)
 
     return vel
+
+
 if __name__ == "__main__":
     run()
