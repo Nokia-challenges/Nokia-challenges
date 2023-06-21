1db298ad295fb964f04e36922b4607d240dfc7ec
unknown <lindaniele25@gmail.com>
2023-06-21 12:17:48 +0200

    Soluzione 4 elegantissima (lindaniele)

--- challenge4_palindromes/solution.py
+++ challenge4_palindromes/solution.py
@@ -1,6 +1,17 @@
 def run(s):
-    return ""
+    longest_word = s[0]
+    longest_i = 0
+    length = len(s)
 
+    for index in range(length):
+        for i in range(1, min(index+1, length-index)):
+            if s[index-i] != s[index+i]:
+                break
+            elif i>longest_i:
+                longest_i = i
+                longest_word = s[index-i:index+i+1]
+
+    return longest_word
 
 if __name__ == "__main__":
-    run(s="nokiaikchallenges")
+    run(s="nokiaikchallenaaaaaaaaaaagesbbbbbbbbbbbbbbbbbbbbbb")
