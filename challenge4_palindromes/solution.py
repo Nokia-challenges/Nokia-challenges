36ea5eeab5e30e060197c9e2ae5730157dca7ce1
FrancescoCasasole <francesco.casasole@gmail.com>
2023-06-21 14:40:53 +0200

    quarta challenge quarto tentativo

--- challenge4_palindromes/solution.py
+++ challenge4_palindromes/solution.py
@@ -1,20 +1,37 @@
 
 def run(s):
+    def run(s):
 
-    parola=""
-    lenmax=0
-    for i in range(len(s)):
-        parola=s[i]
-        for j in range(i+1,len(s)):
-            parola=parola+s[j]
-            if parola==parola[::-1]:
-                if len(parola)>lenmax:
-                    palindroma=parola
-                    lenmax=len(parola)
+        stringamax = ""
+        for i in range(len(s)):
+            for j in range(min(i + 1, len(s) - i + 1)):
+                stringa = s[i - j:i + j + 1:]
+                if stringa == stringa[::-1]:
+                    if len(stringa) > len(stringamax):
+                        stringamax = stringa
 
+        for i in range(len(s)):
+            if i > 0:
+                b = i
+                e = i + 1
+                while b >= 0 and e < len(s):
+                    stringa = s[b:e + 1]
+                    if stringa == stringa[::-1] and len(stringa) > len(stringamax):
+                        stringamax = stringa
+                    b -= 1
+                    e += 1
+            if i < len(s) - 1:
+                b = i - 1
+                e = i
+                while b >= 0 and e < len(s):
+                    stringa = s[b:e + 1]
+                    if stringa == stringa[::-1] and len(stringa) > len(stringamax):
+                        stringamax = stringa
+                    b -= 1
+                    e += 1
 
-    return palindroma
+        return stringamax
 
+    if __name__ == "__main__":
+        run(s="nokiaikchallenges")
 
-if __name__ == "__main__":
-    run(s="nokiaikchallenges")
