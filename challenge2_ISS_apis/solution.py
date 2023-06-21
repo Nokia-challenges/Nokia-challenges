f0acf4aa2a4d464fff4e08a255f30821c71c6c3f
unknown <lindaniele25@gmail.com>
2023-06-21 10:33:09 +0200

    Soluzione 2

--- challenge2_ISS_apis/solution.py
+++ challenge2_ISS_apis/solution.py
@@ -4,14 +4,23 @@ from math import radians, cos, sin, asin, sqrt
 
 url = "http://api.open-notify.org/iss-now.json"
 
-def haversine_distance(coo1, coo2):
-    lon1, lat1 = map(float, coo1.values())
-    lon2, lat2 = map(float, coo2.values())
+def coo_to_radians(coo):
+    return map(
+        radians, 
+        map(float, coo.values())
+    )
 
-    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+def haversine_distance(coo1, coo2):
+    # coo1 = map(float, coo1.values())
+    # coo2 = map(float, coo2.values())
+
+    lon1, lat1 = coo_to_radians(coo1)
+
+    lon2, lat2 = coo_to_radians(coo2)
 
     dlon = lon2 - lon1
-    dlat = lat1 - lat1
+    dlat = lat2 - lat1
 
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
     c = 2 * asin(sqrt(a)) 
@@ -24,15 +33,13 @@ def run():
     # start = time.time()
     coordinates1 = requests.get(url).json()["iss_position"]
 
-    time.sleep(1)
+    time.sleep(5)
     # end = time.time()
     coordinates2 = requests.get(url).json()["iss_position"]
     
-
-
     distance = haversine_distance(coordinates1, coordinates2)
     # print(distance/(end-start)*3600)
-    return distance*3600
+    return (distance/5)*3600
 
 
 if __name__ == "__main__":
