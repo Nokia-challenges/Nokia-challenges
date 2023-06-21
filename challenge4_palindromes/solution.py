0b410672ef21bc02fad2603050f9b26a9c9b6c03
unknown <lindaniele25@gmail.com>
2023-06-21 14:35:26 +0200

    Soluzione 4 bella

--- challenge4_palindromes/solution.py
+++ challenge4_palindromes/solution.py
@@ -1,17 +1,17 @@
 def run(s):
     longest_word = s[0]
-    longest_i = 0
     length = len(s)
-
     for index in range(length):
         for i in range(1, min(index+1, length-index)):
             if s[index-i] != s[index+i]:
+                if s[index-i] != s[index]:
+                    break
+                elif len(palindrome:=s[index-i:index+i]) > len(longest_word):
+                    longest_word = palindrome
                 break
-            elif i>longest_i:
-                longest_i = i
-                longest_word = s[index-i:index+i+1]
-
-    return longest_word
+            elif len(palindrome:=s[index-i:index+i+1])>len(longest_word):
+                longest_word = palindrome
+    return s[-2:] if len(longest_word)<2 and s[-1]==s[-2] else longest_word
 
 if __name__ == "__main__":
-    run(s="nokiaikchallenaaaaaaaaaaagesbbbbbbbbbbbbbbbbbbbbbb")
+    run(s="ffdffffcefdd")
