5ffd932fbd82eaf6cb3b59cee7125dda093cd837
diecl <dieclementi.go@gmail.com>
2023-06-21 10:11:23 +0200

    prima prova soluzione

--- challenge1_brute_forcing/solution.py
+++ challenge1_brute_forcing/solution.py
@@ -1,6 +1,25 @@
+import urllib.request
+
+#url = "https://github.com/napolux/paroleitaliane/blob/master/paroleitaliane/660000_parole_italiane.txt"
+from utils import check_password
+
+
 def run():
+    with urllib.request.urlopen('https://raw.githubusercontent.com/napolux/paroleitaliane/master/paroleitaliane/660000_parole_italiane.txt') as response:
+        while True:
+            parola = response.readline()
+            riga = parola.decode()
+            riga = riga.rstrip()
+            if check_password(riga) == True:
+                print(riga)
+                file_uno = open("passphrase.txt", "w")
+                file_uno.write(riga)
+                file_uno.close()
+                break
+
     pass
 
 
 if __name__ == "__main__":
     run()
+
