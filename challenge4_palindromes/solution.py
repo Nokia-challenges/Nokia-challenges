081c598d47877238e99760fcf992e99530cae596
scrim_2p0801v <scrimaandre@gmail.com>
2023-06-21 11:49:40 +0200

    soluzione 1

--- challenge4_palindromes/solution.py
+++ challenge4_palindromes/solution.py
@@ -1,5 +1,18 @@
 def run(s):
-    return ""
+    palindroma=""
+
+    for i in range(1,len(s)-1):
+        if s[i-1]==s[i+1]:
+            pos=i
+            palindroma=s[pos-1]+s[pos]+s[pos+1]
+
+    for j in range(2,len(s)):
+        if s[pos-j]==s[pos+j]:
+            palindroma=s[pos-j]+palindroma+s[pos+j]
+        if s[pos-j]!=s[pos+j]:
+            break
+
+    print(palindroma)
 
 
 if __name__ == "__main__":
