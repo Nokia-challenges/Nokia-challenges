2691f95a53c9d155de2f89f6fc37a7b1baff9146
saragambino <saragambino30@gmail.com>
2023-06-21 12:05:50 +0200

    challenge 2

--- challenge4_palindromes/solution.py
+++ challenge4_palindromes/solution.py
@@ -1,5 +1,18 @@
 def run(s):
-    return ""
+    palindroma=""
+    lenmax=0
+
+    for i in range(len(s)):
+        parola=s[i]
+        for j in range(i+1, len(s)):
+            parola=parola+s[j]
+            if parola==parola[::-1]:
+                if len(parola)>=lenmax:
+                    palindroma=parola
+                    lenmax=len(parola)
+    print(palindroma)
+
+
 
 
 if __name__ == "__main__":
